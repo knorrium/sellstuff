0ebc13eedc97bbfb32dbdd881c95945217d66ad4
Felipe Knorr Kuhn <git@knorrium.info>
2012-08-26 18:46:43 -0300

    Working items list and item details

--- routes/routes.js
+++ routes/routes.js
@@ -9,8 +9,40 @@ module.exports = function (app) {
     });
 
     app.get('/items', function (req, res) {
-        res.render('items', {
-            title: 'Items'
+        var Item = new app.Schema({
+                title: String
+            }),
+            itemModel = app.mongoose.model('Item', Item);
+
+        itemModel.find({}, function (err, items) {
+            res.render('items', {
+                title: 'Items for sale',
+                items: items
+            });
+        });
+    });
+
+    app.get('/item/:id', function (req, res) {
+        var Item = new app.Schema({
+                title: String,
+                description: String,
+                price: String,
+                tags: [String],
+                pictures: [String]
+            }),
+            itemModel = app.mongoose.model('Item', Item);
+
+        itemModel.findOne({'_id': req.params.id}, function (err, item) {
+            if (item !== null) {
+                res.render('item', {
+                    title: 'Item details',
+                    item: item
+                });
+            } else {
+                res.send({
+                    'error': 'item not found'
+                });
+            }
         });
     });
 
